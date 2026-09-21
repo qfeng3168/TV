4a8f37b1e7aa05d50a40bb4b359331098e8578f6
qfeng3168 <q81801@qq.com>
2026-09-21 12:51:02 +0800

    fix(live): ChannelAdapter 补回被覆盖的 EpgData/LinearLayout/Nullable import

--- app/src/leanback/java/com/fongmi/android/tv/ui/adapter/ChannelAdapter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/adapter/ChannelAdapter.java
@@ -3,12 +3,14 @@ package com.fongmi.android.tv.ui.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.fongmi.android.tv.bean.Channel;
+import com.fongmi.android.tv.bean.EpgData;
 import com.fongmi.android.tv.databinding.AdapterChannelBinding;
 
 import java.time.ZoneId;
